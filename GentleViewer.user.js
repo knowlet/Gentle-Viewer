9d245beccdf6b10b67f7388eae6d3305d674cf85
knowlet <knowlet3389@gmail.com>
2015-04-18 19:22:24 +0800

    fix issue

--- GentleViewer.user.js
+++ GentleViewer.user.js
@@ -19,18 +19,18 @@
     };
 
     Gallery.prototype = {
-		checkFunctional: function() {
-			return this.imgNum !== 0;
-		},
+        imgList: [],
+        checkFunctional: function() {
+            return this.imgNum !== 0;
+        },
         loadPageUrls: function(element) {
-			var self = this;
             [].forEach.call(element.querySelectorAll("a[href]"), function(item) {
                 var ajax = new XMLHttpRequest();
                 ajax.onreadystatechange = function() {
                     if (4 == ajax.readyState && 200 == ajax.status) {
                         var imgNo = parseInt(ajax.responseText.match("startpage=(\\d+)").pop());
                         var src = (new DOMParser()).parseFromString(ajax.responseText, "text/html").getElementById("img").src;
-                        self.imgList[imgNo-1].src = src;
+                        Gallery.prototype.imgList[imgNo-1].src = src;
                     }
                 };
                 ajax.open("GET", item.href);
@@ -42,8 +42,8 @@
             for (var i = 1; i < this.pageNum; ++i) {
                 var ajax = new XMLHttpRequest();
                 ajax.onreadystatechange = function() {
-                    if (4 == ajax.readyState && 200 == ajax.status) {
-                        var dom = (new DOMParser()).parseFromString(ajax.responseText, "text/html");
+                    if (4 == this.readyState && 200 == this.status) {
+                        var dom = (new DOMParser()).parseFromString(this.responseText, "text/html");
                         LoadPageUrls(dom.getElementById("gdt"));
                     }
                 };
@@ -55,27 +55,28 @@
             while (gdt.firstChild && gdt.firstChild.className)
                 gdt.removeChild(gdt.firstChild);
         },
-        generateImg: function() {
-			this.imgList = [];
+        generateImg: function(callback) {
             for (var i = 0; i < this.imgNum; ++i) {
                 var img = document.createElement("img");
                 img.setAttribute("src", "//ehgt.org/g/roller.gif");
                 this.imgList.push(img);
                 gdt.appendChild(img);
             }
+            callback && callback();
         }
     };
     var g = new Gallery(lpPage, lpImg);
-	if (g.checkFunctional()) {
-		g.generateImg();
-		g.loadPageUrls(gdt);
-		g.claenGDT();
-		if (g.pageNum)
-			g.getNextPage();
-	}
-	else {
-		alert("There are some issue in the script\nplease open an issue on Github");
-		window.open("https://github.com/knowlet/Gentle-Viewer/issues");
-	}
+    if (g.checkFunctional()) {
+        g.generateImg(function() {
+            g.loadPageUrls(gdt);
+            g.claenGDT();
+            if (g.pageNum)
+                g.getNextPage();
+        });
+    }
+    else {
+        alert("There are some issue in the script\nplease open an issue on Github");
+        window.open("https://github.com/knowlet/Gentle-Viewer/issues");
+    }
     
 })(document.querySelectorAll("td[onclick^=sp]").length / 2, parseInt(gdd.querySelector("#gdd tr:nth-child(n+6) td.gdt2").textContent.split(" ")[0]));
